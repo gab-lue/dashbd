7a0eb6fe93b5f67cf251d44e9f7f7b0ae7d419a8
gab <gabriel.luepke@outlook.de>
2023-02-26 20:27:06 +0100

    0.1
    version 0.1

--- MyApp.py
+++ MyApp.py
@@ -7,18 +7,18 @@ import plotly.figure_factory as ff
 from io import StringIO
 import os.path
 import time
-
+from ipyvizzu import Chart, Data, Config
 
 # Define function to read Excel file
 
-#global
-data = None
+#global#data = None
 
 # Define Streamlit app
 def app():
-    global data
+   
+
     
-    @st.cache(allow_output_mutation=True)
+    @st.cache_data
     def read_excel_file(filename):
         df = pd.read_excel(filename, engine='openpyxl',
                                    sheet_name='Ranking',
@@ -33,35 +33,41 @@ def app():
         file = st.file_uploader("Upload Excel file", type=["xlsx"])
     
     # Check if file is uploaded
-    print(data)
-    if file is not None:
+    #print(data)
+    #if file is not None:
         # Read Excel file
         #read df and split it into 4 lists
-        data = read_excel_file(file)
-        data.info()
-        st.write("file loading")
+    data = read_excel_file("Unit 1 - Ranking.xlsx")
     
     
        
-    if data is not None:
+    #if data is not None:
         #4 new dfs
-        df_meetings = data.iloc[0:32,0:2]
-        df_held = data.iloc[0:32,3:5]
-        df_agreement = data.iloc[0:32,6:8]
-        df_turnover = data.iloc[0:32,9:11]
+    df_meetings = data.iloc[0:32,0:2]
+    df_held = data.iloc[0:32,3:5]
+    df_agreement = data.iloc[0:32,6:8]
+    df_turnover = data.iloc[0:32,9:11]
+    name_meeting,value_meeting = df_meetings.at[0,2],df_meetings.at[0,3]
+    name_held,value_held = df_held.at[0,5],df_held.at[0,6]
+    name_agree,value_agree = df_agreement.at[0,8],df_agreement.at[0,9]
+    name_turn,value_turn = df_turnover.at[0,11],df_turnover.at[0,12]
+  
+    st.markdown('### Bestleistungen')
+    col1,col2,col3,col4 = st.columns(4)
+    col1.metric("gebuchte Termine:crown:",str(name_meeting),str(value_meeting))
+    col2.metric("abgehaltene Termine:crown:",str(name_held),str(value_held))
+    col3.metric("Vereinbarungen:crown:",str(name_agree),str(value_agree))
+    col4.metric("Umsatz:crown:",str(name_turn),str(value_turn))  
          
          #make plots
-        termine = px.bar(df_meetings,
-                        title="Gebuchte Meetings",
-                        x=2,
-                        y=3,
-                        color=3,
-                        orientation="v",
-                        hover_name=2,
-                        
-                        )
+ 
+    
+    
+        
+        #st.write("file loading")
+        
 
-        abgehalten = px.bar(df_held,
+    abgehalten = px.bar(df_held,
                         title="abgehaltene Termine",
                         x=5,
                         y=6,
@@ -70,7 +76,7 @@ def app():
                         hover_name=5
                         )
 
-        vereinbarungen = px.bar(df_agreement,
+    vereinbarungen = px.bar(df_agreement,
                         title="Vereinbarungen",
                         x=8,
                         y=9,
@@ -79,7 +85,7 @@ def app():
                         hover_name=9            
                         )
 
-        turnover = px.bar(df_turnover,
+    turnover = px.bar(df_turnover,
                         title="UMSATZ",
                         x=11,
                         y=12,
@@ -89,30 +95,62 @@ def app():
                         )
         
         #show plots
+    #rows data
+    
+    c1,c2 = st.columns((50,50))
+    with c1:
         
-        st.plotly_chart(termine)
-        st.plotly_chart(abgehalten)
+        #st.write(print(name_meeting,value_meeting))
+        #st.markdown('#### Termine gebucht')
+        
+        fig = px.bar(df_meetings, x=2, y=3,orientation="v",color=3,
+                     title="gebuchte Termine",
+                     opacity=0.8,
+                     hover_name=3,
+                     hover_data=(),
+                     
+                     barmode="relative")
+        
+        st.write(fig)
+        fig.update_layout(showlegend=False)
+        #st.markdown('### abgehaltene Termine')
+        st.plotly_chart(abgehalten,theme="streamlit")
+    with c2:
         st.plotly_chart(vereinbarungen)
         st.plotly_chart(turnover)
+   
+        
             
-    else:
+    #else:
         # Add message to tell user to upload file
-        st.write("Please upload an Excel file.")
+        #st.write("Please upload an Excel file.")
         
-        
+      
         
 
 
-### script start###
+################################### script start#########################
 #config
 st.set_page_config(page_title="Unit 1 Dashboard",
                    page_icon=":bar_chart:",
-                   layout="wide"
+                   layout="wide",
+                   initial_sidebar_state='collapsed'
                    )  
-  
+#css
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', 
+                unsafe_allow_html=True)
+st.header("Alle Wichtigen Zahlen für diesen Monat")
+     
+st.sidebar.header('Dashboard `version1`')
+
+#rows shortcuts
+
+
+
 #überschrift
-st.header("Alle Wichtigen Zahlen")
-st.subheader("Top Performer:")   
 
 app()
+
+
 
