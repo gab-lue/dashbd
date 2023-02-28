9bb7a26b5de72fd848b6b5d5f61417105f54394a
gab <gabriel.luepke@outlook.de>
2023-02-28 08:01:50 +0100

    Update MyApp.py
    pcs

--- MyApp.py
+++ MyApp.py
@@ -141,7 +141,7 @@ st.set_page_config(page_title="Unit 1 Dashboard",
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', 
                 unsafe_allow_html=True)
-st.header("Aktueller Stand der Zahlen / PCS / Monat Februar")
+st.header("Aktueller Stand der Zahlen für PCS / Monat Februar")
      
 st.sidebar.header('Dashboard `version1`')
 
